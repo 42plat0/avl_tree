ebfd3b05b470ebf23abd46802373510058a42152
penguin-wings <gytisnaujasis@icloud.com>
2025-02-16 15:15:57 +0200

    BST create, insert and print funcs

--- myavltree.h
+++ myavltree.h
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// Define datatype used
+typedef int dt;
+
+struct Node {
+    dt data;
+    struct Node *right;
+    struct Node *left;
+};
+
+typedef struct Node Node;
+
+// Create node
+Node* createNode(dt);
+
+// Insert Node
+Node* ins(Node*, dt);
+
+// Search Node
+Node* search(dt);
+
+// Delete node
+Node* del(Node*, dt);
+
+// Prints tree in-order successor - KVD 
+void printTreeInOrder(Node*);
