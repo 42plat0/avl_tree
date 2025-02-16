38ce1a04f3818f5481faa6ad9906b061d84682d5
penguin-wings <gytisnaujasis@icloud.com>
2025-02-16 15:56:51 +0200

    Add del of 0 or 1 child func BST

--- Makefile
+++ Makefile
@@ -4,5 +4,10 @@ run: myavltree
 make: myavltree.c
 	gcc myavltree.c -o myavltree
 
+gdb: 
+	rm -rf myavltree
+	gcc -g -o myavltree myavltree.c
+	gdb ./myavltree
+	
 clean:
 	rm -rf myavltree
--- myavltree.c
+++ myavltree.c
@@ -1,15 +1,22 @@
 #include "myavltree.h"
 
 int main(){
+    // Creating the following BST
+    //      50
+    //     /  \
+    //    30   70
+    //   / \   / \
+    //  20 40 60 80
+
     Node* root = createNode(50);
-    ins(root, 13);
+    ins(root, 30);
+    ins(root, 20);
+    ins(root, 40);
+    ins(root, 70);
     ins(root, 60);
+    ins(root, 80);
+    del(root, 20); 
     printTreeInOrder(root);
-    printf("\n");
-    printf(search(root, 19) != NULL ? "Found\n"
-                                    : "Not Found\n");
-    printf(search(root, 60) != NULL ? "Found\n"
-                                    : "Not Found\n");
     return 0;
 }
 
@@ -71,6 +78,40 @@ Node* search(Node* root, dt data){
     else if(root->data > data){
         return search(root->left, data);
     }
+}
+
+Node* del(Node* root, dt data){
+    // Base case
+    if (root == NULL){
+        return NULL;
+    }
+
+    // Go right if data is bigger than curr node
+    if (root->data < data){
+        root->right = del(root->right, data);
+    }
+    // Go left if data is smaller than curr node
+    else if(root->data > data){
+        root->left = del(root->left, data);
+    }
+    // Found node to be deleted
+    else {
+        // has 0 or only right child
+        if(root->left == NULL){
+            Node* temp = root->right; // Can be valid node or NULL 
+            free(root);
+            return temp;
+        }
+        
+        // has 0 or only left child
+        if(root->right == NULL){
+            Node* temp = root->left; // Can be valid node or NULL 
+            free(root);
+            return temp;
+        }
+    }
+
+    return root;
 }
 
 void printTreeInOrder(Node* root)
